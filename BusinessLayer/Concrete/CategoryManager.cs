bc70c3744a04b50638fddfb936d82fc4f364a9fc
Abdullah Balıkçı <abdblk2525@gmail.com>
2022-10-08 21:19:37 +0300

    CategoryFinished

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLayer.Abstract;
+using DataAccesLayer.Abstract;
+using DataAccesLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        //   GenericRepository<Category> repo = new GenericRepository<Category>();
+        ICategoryDal _categoryDal;
+       
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+        public List<Category> GetList()
+        {
+            return _categoryDal.List();
+        }
+
+       
+
+        public void CategoryAdd(Category category)
+        {
+            _categoryDal.Insert(category);
+        }
+    }
+}
---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator:AbstractValidator<Category>
+    {
+
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Boş Geçemezsiniz.");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Boş geçemezsiniz.");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Lütfen en az 3 karakter giriniz.");
+            RuleFor(X => X.CategoryName).MaximumLength(20).WithMessage("lÜTFEN 20 KARAKTERDEB FAZLA DEĞER GİRİŞİ YAPMAYIN.");
+        }
+
+    }
+}
+    
